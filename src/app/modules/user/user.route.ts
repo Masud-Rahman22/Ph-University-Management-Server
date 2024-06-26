5ebad9d0d8186780d1bce113b6019ddf8d53dbbd
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-06-26 15:07:10 +0600

    modify the student zod validate file

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { userControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
-import { studentValidationSchema } from '../student/student.validate';
+import { createStudentValidationSchema } from '../student/student.validate';
 
 const router = express.Router();
 
 
 
-router.post('/create-student', validateRequest(studentValidationSchema), userControllers.createStudent);
+router.post('/create-student', validateRequest(createStudentValidationSchema), userControllers.createStudent);
 
 export const UserRoutes = router;
