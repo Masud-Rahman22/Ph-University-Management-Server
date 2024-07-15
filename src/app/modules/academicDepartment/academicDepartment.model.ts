f6ec02a0614a7c320a603d7bacb7778d5593bb65
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-15 22:40:36 +0600

    added pre hook middlewares for necessary validations

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,5 +18,28 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+
+    const isDepartmentExist = await AcademicDepartment.findOne({
+        name: this.name
+    })
+
+    if (isDepartmentExist) {
+        throw new Error("department already exists")
+    }
+    next()
+})
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+    const query = this.getQuery()
+
+    const isDepartmentExist = await AcademicDepartment.findOne(query)
+
+    if (!isDepartmentExist) {
+        throw new Error("Department does not exist")
+    }
+    next()
+})
+
 
 export const AcademicDepartment = model<TAcademicDepartment>('AcademicDepartment', academicDepartmentSchema)
