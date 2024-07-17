a6dc633c06029730c855dc41185252487dc11477
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-18 02:01:53 +0600

    all types of error including zod , mongoose validation , cast error , duplicate error , unhandled rejection , uncaught exception is handled

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,35 @@
+import { Server } from 'http';
+import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
-import mongoose from 'mongoose';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+
+    server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
+
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unhandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
