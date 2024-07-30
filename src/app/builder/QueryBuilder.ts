9283303f99171557d574e73fa3a920a2e6f731d3
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-30 15:10:50 +0600

    completed the queryBuilder with paginate , sort , limiting fields methods too

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -28,5 +28,25 @@ class QueryBuilder<T> {
         this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
         return this;
     }
+
+    sort() {
+        const sort = (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
+        this.modelQuery = this.modelQuery.sort(sort as string)
+        return this
+    }
+
+    paginate() {
+        const page = Number(this?.query?.page) || 1;
+        const limit = Number(this?.query?.limit) || 10;
+        const skip = (page - 1) * limit;
+        this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+        return this;
+    }
+
+    fields() {
+        const fields = (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
+        this.modelQuery = this.modelQuery.select(fields);
+        return this;
+    }
 }
 export default QueryBuilder;
