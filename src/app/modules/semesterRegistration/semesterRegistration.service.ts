382c7c4fcad1cd2cb9ec94e07de4ac17b44e7a4f
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-08-02 02:25:13 +0600

    successfully created a semester registration

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -2,6 +2,7 @@ import httpStatus from "http-status"
 import AppError from "../../errors/AppError"
 import { AcademicSemester } from "../academicSemester/academicSemester.model"
 import { TSemesterRegistration } from "./semesterRegistration.interface"
+import { SemesterRegistration } from "./semesterRegistration.model"
 
 const createSemesterRegistrationIntoDB = async (payload: TSemesterRegistration) => {
     const academicSemester = payload?.academicSemester
@@ -9,6 +10,12 @@ const createSemesterRegistrationIntoDB = async (payload: TSemesterRegistration) 
     if (!isAcademicSemesterExists) {
         throw new AppError(httpStatus.NOT_FOUND, "this academic semester does not exist")
     }
+    const isSemesterRegistrationExists = await SemesterRegistration.findOne({ academicSemester })
+    if (isSemesterRegistrationExists) {
+        throw new AppError(httpStatus.CONFLICT, "this semester is already registered")
+    }
+    const result = await SemesterRegistration.create(payload)
+    return result;
 }
 
 const getAllSemesterRegistrationsFromDB = async () => {
