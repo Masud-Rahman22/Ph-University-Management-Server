bb2c4df831d0db4ea604884b0121e6b5c2de021e
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-08-02 02:10:39 +0600

    working on semester registration service

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,5 +1,14 @@
-const createSemesterRegistrationIntoDB = async (payload) => {
+import httpStatus from "http-status"
+import AppError from "../../errors/AppError"
+import { AcademicSemester } from "../academicSemester/academicSemester.model"
+import { TSemesterRegistration } from "./semesterRegistration.interface"
 
+const createSemesterRegistrationIntoDB = async (payload: TSemesterRegistration) => {
+    const academicSemester = payload?.academicSemester
+    const isAcademicSemesterExists = await AcademicSemester.findById(academicSemester)
+    if (!isAcademicSemesterExists) {
+        throw new AppError(httpStatus.NOT_FOUND, "this academic semester does not exist")
+    }
 }
 
 export const SemesterRegistrationService = {
