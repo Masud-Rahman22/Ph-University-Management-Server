8af301063094c174ac010dddce8113f1d74f07b9
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-08-04 18:00:01 +0600

    compared hashed passowrd with plain password

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,7 +2,7 @@ import httpStatus from "http-status"
 import AppError from "../../errors/AppError"
 import { User } from "../user/user.model"
 import { TLoginUser } from "./auth.interface"
-
+import bcrypt from 'bcrypt';
 const loginUser = async (payload: TLoginUser) => {
     const doesUserExist = await User.findOne({ id: payload?.id })
     if (!doesUserExist) {
@@ -20,6 +20,12 @@ const loginUser = async (payload: TLoginUser) => {
     if (userStatus) {
         throw new AppError(httpStatus.FORBIDDEN, 'this user is blocked!')
     }
+
+    const isPasswordMatched = await bcrypt.compare(payload?.password,
+        doesUserExist?.password
+    )
+    console.log(isPasswordMatched)
+    return {};
 }
 
 export const AuthServices = {
